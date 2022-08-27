07fc0af20f717f73f8cd16065e047a11bc2c210a
Brodie Peif <brodiep21@hotmail.com>
2022-08-27 11:30:17 -0400

    working text box for zip

--- go.mod
+++ go.mod
@@ -2,10 +2,18 @@ module github.com/brodiep21/ngicGui
 
 go 1.19
 
-require fyne.io/fyne/v2 v2.2.3
+require (
+	fyne.io/fyne/v2 v2.2.3
+	github.com/rocketlaunchr/google-search v1.1.4
+)
 
 require (
 	fyne.io/systray v1.10.1-0.20220621085403-9a2652634e93 // indirect
+	github.com/PuerkitoBio/goquery v1.5.0 // indirect
+	github.com/andybalholm/cascadia v1.0.0 // indirect
+	github.com/antchfx/htmlquery v1.0.0 // indirect
+	github.com/antchfx/xmlquery v1.0.0 // indirect
+	github.com/antchfx/xpath v1.0.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/fredbi/uri v0.0.0-20181227131451-3dcfdacbaaf3 // indirect
 	github.com/fsnotify/fsnotify v1.5.4 // indirect
@@ -14,14 +22,20 @@ require (
 	github.com/fyne-io/image v0.0.0-20220602074514-4956b0afb3d2 // indirect
 	github.com/go-gl/gl v0.0.0-20211210172815-726fda9656d6 // indirect
 	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20211213063430-748e38ca8aec // indirect
+	github.com/gobwas/glob v0.2.3 // indirect
+	github.com/gocolly/colly/v2 v2.0.1 // indirect
 	github.com/godbus/dbus/v5 v5.1.0 // indirect
 	github.com/goki/freetype v0.0.0-20181231101311-fa8a33aabaff // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/gopherjs/gopherjs v1.17.2 // indirect
 	github.com/jsummers/gobmp v0.0.0-20151104160322-e2ba15ffa76e // indirect
+	github.com/kennygrant/sanitize v1.2.4 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
 	github.com/srwiley/oksvg v0.0.0-20200311192757-870daf9aa564 // indirect
 	github.com/srwiley/rasterx v0.0.0-20200120212402-85cb7272f5e9 // indirect
 	github.com/stretchr/testify v1.7.2 // indirect
+	github.com/temoto/robotstxt v1.1.1 // indirect
 	github.com/tevino/abool v1.2.0 // indirect
 	github.com/yuin/goldmark v1.4.0 // indirect
 	golang.org/x/image v0.0.0-20220601225756-64ec528b34cd // indirect
@@ -29,6 +43,9 @@ require (
 	golang.org/x/net v0.0.0-20210805182204-aaa1db679c0d // indirect
 	golang.org/x/sys v0.0.0-20220412211240-33da011f77ad // indirect
 	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/time v0.0.0-20201208040808-7e3f01d25324 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.26.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	honnef.co/go/js/dom v0.0.0-20210725211120-f030747120f2 // indirect
 )
--- main.go
+++ main.go
@@ -1,65 +1,92 @@
 package main
 
 import (
-	"fyne.io/fyne/v2/theme"
-	// "fyne.io/fyne/v2"
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/dialog"
+	// "fyne.io/fyne/v2/theme"
 	// "fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/driver/desktop"
 	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
 	// "image/color"
-	// "github.com/rocketlaunchr/google-search"
+	"context"
+	"errors"
+	"github.com/rocketlaunchr/google-search"
+	"strings"
 )
 
 func main() {
-	myApp := app.New()
-	win := myApp.NewWindow("NGIC")
-	// g := color.NRGBA{R: 0, G: 180, B: 0, A: 255}
-	username := &widget.Entry{PlaceHolder: "Username"}
-	// text1 := canvas.NewText("Hello", g)
-	// text2 := canvas.NewText("There", g)
-	// text2.Move(fyne.NewPos(20, 20))
-	// content := container.NewWithoutLayout(text1, text2)
+	a := app.New()
+	win := a.NewWindow("NGIC")
+
+	zip := widget.NewEntry()
+	zip.SetPlaceHolder("Enter Zip")
+
+	button := widget.NewButton("Get Zip", func() {
+		tax, err := searchForTaxRate(zip.Text)
+		if err != nil {
+			dialog.ShowError(err, win)
+		} else {
+			zip.SetText(tax)
+		}
+	})
+
+	//sets a system tray that will only close from the tray/toolbar if using a desktop
+	if desk, ok := a.(desktop.App); ok {
+		m := fyne.NewMenu("NGICapp",
+			fyne.NewMenuItem("Show", func() {
+				win.Show()
+			}))
+		desk.SetSystemTrayMenu(m)
+	}
 	menu := container.NewGridWithRows(3,
 		container.NewGridWithColumns(3,
-			layout.NewSpacer(),
+			// layout.NewSpacer(),
 			container.NewVBox(
-				username,
-				&widget.Button{Text: "Login", Icon: theme.ConfirmIcon()},
+				zip,
+				button,
+				layout.NewSpacer(),
 			),
 		),
 	)
-	// content := container.New(layout.NewGridLayout(2), text1, text2)
 
-	// win.SetContent(content)
+	win.SetContent(widget.NewLabel("Fyne System Tray"))
+
+	// win.SetContent(hBox)
 	win.SetContent(menu)
+
+	//when using the X close button, the app is only closed and minimuzed to the tray
+	win.SetCloseIntercept(func() {
+		win.Hide()
+	})
 	win.ShowAndRun()
 }
 
-// func searchForTaxRate(zipCode string) (string, error) {
+func searchForTaxRate(zipCode string) (string, error) {
 
-// 	search, err := googlesearch.Search(context.TODO(), "tax rate of "+zipCode)
-// 	if err != nil {
-// 		return "", err
-// 	}
+	search, err := googlesearch.Search(context.TODO(), "tax rate of "+zipCode)
+	if err != nil {
+		return "", err
+	}
 
-// 	for _, v := range search {
-// 		if strings.Contains(v.Description, "%") {
-// 			pos := strings.Index(v.Description, "%")
-// 			if pos == -1 {
-// 				break
-// 			}
-// 			tax := v.Description[(pos - 6) : pos+1]
-// 			tax = strings.TrimPrefix(tax, "is ")
-// 			tax = strings.TrimPrefix(tax, " is ")
-// 			tax = strings.TrimPrefix(tax, "is")
-// 			tax = strings.TrimPrefix(tax, " ")
-// 			tax = strings.TrimPrefix(tax, "s")
-// 			tax = strings.TrimPrefix(tax, "a")
-// 			return tax, nil
-// 		}
-// 	}
+	for _, v := range search {
+		if strings.Contains(v.Description, "%") {
+			pos := strings.Index(v.Description, "%")
+			if pos == -1 {
+				break
+			}
+			tax := v.Description[(pos - 6) : pos+1]
+			tax = strings.TrimPrefix(tax, "is ")
+			tax = strings.TrimPrefix(tax, " is ")
+			tax = strings.TrimPrefix(tax, "is")
+			tax = strings.TrimPrefix(tax, " ")
+			tax = strings.TrimPrefix(tax, "s")
+			tax = strings.TrimPrefix(tax, "a")
+			return tax, nil
+		}
+	}
 
-// 	return "", errors.New("could not locate tax")
-// }
+	return "", errors.New("could not locate tax")
+}
