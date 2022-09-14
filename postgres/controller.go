b66052bc20bf0efccc0e6091b4ded35ef5abebc0
Brodie Peif <brodiep21@hotmail.com>
2022-09-13 20:39:11 -0400

    In process controller/handler

--- postgres/controller.go
+++ postgres/controller.go
@@ -1,12 +1,15 @@
 package postgres
 
 import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
 	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
 	"net/http"
 	"time"
-	"fmt"
 )
+
 type App struct {
 	Router *mux.Router
 	DB     *sql.DB
@@ -16,25 +19,54 @@ const (
 	host   = "localhost"
 	port   = "5432"
 	user   = "b21"
-	dbname = string
+	dbname = ""
 )
 
-func (a *App) Initialize(password string) error {
+func (a *App) Init(password string) error {
 	var psql = fmt.Sprintf("host=%s port=%d user=%s "+
 		"password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbname)
 
-	a.DB, err := sql.Open("postgres", psql)
+	var err error
+	a.DB, err = sql.Open("postgres", psql)
 	if err != nil {
 		return err
 	}
 
+	// a.Run()
 	a.DB.SetMaxIdleConns(5)
 	a.DB.SetMaxOpenConns(5)
 	a.DB.SetConnMaxIdleTime(5 * time.Second)
-	a.DB.SetConnMaxLifeTime(20 * time.Second)
+	a.DB.SetConnMaxLifetime(20 * time.Second)
+
+	a.Router = mux.NewRouter()
+	a.InitRoutes()
+	return nil
+
 }
 
-func http(w http.ResponseWriter, *r http.Request) {
-
+func (a *App) Run(address string) error {
+	err := http.ListenAndServe(":"+address, a.Router)
+	if err != nil {
+		return err
+	}
+	return nil
 }
+
+func HttpErrorResponse(w http.ResponseWriter, Rcode int, message string) {
+	JsonResponse(w, Rcode, map[string]string{"error": message})
+}
+
+func JsonResponse(w http.ResponseWriter, Rcode int, info interface{}) {
+	response, _ := json.Marshal(info)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(Rcode)
+	w.Write(response)
+}
+
+// func (a *App) GetRateByZip
+
+// func (a *App) InitRoutes() {
+// 	a.Router.HandleFunc(path, f)
+// }
--- postgres/query.go
+++ postgres/query.go
@@ -1,42 +1,73 @@
 package postgres
 
 import (
-	"time"
 	"database/sql"
+	"fmt"
 	_ "github.com/lib/pq"
+	"time"
 )
 
 type Labor struct {
-	Zip           string `json:"ZIP"`
-	County        string `json:"County"`
-	BodyRate      string `json:"BodyRate"`
-	PaintRate     string `json:"PaintRate"`
-	MechRate      string `json:"MechRate"`
-	FrameRate     string `json:"FrameRate"`
-	PaintSupplies string `json:"PaintSupplies"`
-	Createdat time.Time `json:"Createdat"`
+	Zip           string    `json:"ZIP"`
+	County        string    `json:"County"`
+	BodyRate      string    `json:"BodyRate"`
+	PaintRate     string    `json:"PaintRate"`
+	MechRate      string    `json:"MechRate"`
+	FrameRate     string    `json:"FrameRate"`
+	PaintSupplies string    `json:"PaintSupplies"`
+	Createdat     time.Time `json:"Createdat"`
 }
 
-func (l *Labor) GetRateByZip(db *sql.DB) error {
-	return db.QueryRow("SELECT ZIP, County, BodyRate, PaintRate, MechRate, FrameRate, PaintSupplies, Createdat FROM labor WHERE ZIP=$1", l.ZIP).Scan(&l.Zip, &l.County, &l.BodyRate, &l.PaintRate,&l.MechRate,&l.FrameRate,&l.PaintSupplies,&l.Createdat)
+func (l *Labor) getRateByZip(db *sql.DB) error {
+	return db.QueryRow("SELECT ZIP, County, BodyRate, PaintRate, MechRate, FrameRate, PaintSupplies, Createdat FROM labor WHERE ZIP=$1", l.ZIP).Scan(&l.Zip, &l.County, &l.BodyRate, &l.PaintRate, &l.MechRate, &l.FrameRate, &l.PaintSupplies, &l.Createdat)
 }
 
-func (l *Labor) GetRateByCounty(db *sql.DB) error {
-	return db.QueryRow("SELECT ZIP, County, BodyRate, PaintRate, MechRate, FrameRate, PaintSupplies, Createdat FROM labor WHERE ZIP=$1", l.County).Scan(&l.Zip, &l.County, &l.BodyRate, &l.PaintRate,&l.MechRate,&l.FrameRate,&l.PaintSupplies,&l.Createdat)
+func (l *Labor) getRateByCounty(db *sql.DB) ([]Labor, error) {
+
+	rows, err := db.Query("SELECT ZIP, County, BodyRate, PaintRate, MechRate, FrameRate, PaintSupplies, Createdat FROM labor WHERE County=$1", l.County).Scan(&l.Zip, &l.County, &l.BodyRate, &l.PaintRate, &l.MechRate, &l.FrameRate, &l.PaintSupplies, &l.Createdat)
+	defer rows.Close()
+
+	labors := []Labor{}
+
+	for rows.Next() {
+		var l labors
+		if err := rows.Scan(&l.Zip, &l.County, &l.BodyRate, &l.PaintRate, &l.MechRate, &l.FrameRate, &l.PaintSupplies, &l.Createdat); err != nil {
+			return nil, err
+		}
+		return labors, nil
+	}
 }
 
-func (l *Labor) UpdateRate) error {
-	result, err := db.Exec("UPDATE labor SET BodyRate=$1, PaintRate=$2, MechRate=$3, FrameRate=$4, PaintSupplies=$5, Createdat=$6 WHERE ZIP=$7", &l.BodyRate, &l.PaintRate,&l.MechRate,&l.FrameRate,&l.PaintSupplies,&l.Createdat, &l.Zip)
+func (l *Labor) updateRate(db *sql.DB) (string, error) {
+	result, err := db.Exec("UPDATE labor SET BodyRate=$1, PaintRate=$2, MechRate=$3, FrameRate=$4, PaintSupplies=$5, Createdat=$6 WHERE ZIP=$7", &l.BodyRate, &l.PaintRate, &l.MechRate, &l.FrameRate, &l.PaintSupplies, &l.Createdat, &l.Zip)
+	if err != nil {
+		return err
+	}
+
+	return fmt.Sprintf("Successfully updated %s rates", l.Zip)
 }
 
+func (l *Labor) createLabor(db *sql.DB) error {
+	result, err := db.QueryRow("INSERT INTO labor (ZIP, County, BodyRate, PaintRate, MechRate, FrameRate, PaintSupplies, Createdat) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)", &l.Zip, &l.County, &l.BodyRate, &l.PaintRate, &l.MechRate, &l.FrameRate, &l.PaintSupplies, &l.Createdat)
+	if err != nil {
+		return err
+	}
+}
 
-CREATE TABLE Labor (
-	ZIP VARCHAR(5) UNIQUE NOT NULL,
-	County VARCHAR(50) NOT NULL,
-	BodyRate VARCHAR(3) NOT NULL,
-	PaintRate VARCHAR(3) NOT NULL,
-	MechRate VARCHAR(3) ,
-	FrameRate VARCHAR(3) ,
-	Paintsupplies VARCHAR(3) NOT NULL,
-	Createdat VARCHAR(50) NOT NULL,
-);
+func (l *Labor) deleteLabor(db *sql.DB) error {
+	result, err := db.QueryRow("ZIP, County, BodyRate, PaintRate, MechRate, FrameRate, PaintSupplies, Createdat WHERE ZIP=$1", l.Zip)
+	if err != nil {
+		return err
+	}
+}
+
+// CREATE TABLE Labor (
+// 	ZIP VARCHAR(5) UNIQUE NOT NULL,
+// 	County VARCHAR(50) NOT NULL,
+// 	BodyRate VARCHAR(3) NOT NULL,
+// 	PaintRate VARCHAR(3) NOT NULL,
+// 	MechRate VARCHAR(3) ,
+// 	FrameRate VARCHAR(3) ,
+// 	Paintsupplies VARCHAR(3) NOT NULL,
+// 	Createdat VARCHAR(50) NOT NULL,
+// );
